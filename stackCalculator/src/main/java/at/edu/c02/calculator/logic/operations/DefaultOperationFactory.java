2f498d62485a26de1dec9b808bb9a902edc92d7c
dominiksammer <dominiksammer@inode.at>
2019-01-12 10:45:24 +0100

    added modulo

--- stackCalculator/src/main/java/at/edu/c02/calculator/logic/operations/DefaultOperationFactory.java
+++ stackCalculator/src/main/java/at/edu/c02/calculator/logic/operations/DefaultOperationFactory.java
@@ -4,18 +4,22 @@ import at.edu.c02.calculator.Calculator;
 import at.edu.c02.calculator.MathOperation;
 import at.edu.c02.calculator.OperationFactory;
 
+import static at.edu.c02.calculator.Calculator.Operation.mod;
+
 public class DefaultOperationFactory implements OperationFactory {
 
 	public  MathOperation getOperation(Calculator.Operation op) {
 		switch (op) {
-		case add:
-			return new Add();
-		case sub:
-			return new Sub();
-		case div:
-			return new Div();
-		case mul:
-			return new Mul();
+			case add:
+				return new Add();
+			case sub:
+				return new Sub();
+			case div:
+				return new Div();
+			case mul:
+				return new Mul();
+			case mod:
+				return new Mod();
 		}
 
 		return null;
---
+++ stackCalculator/src/main/java/at/edu/c02/calculator/logic/operations/Mod.java
@@ -0,0 +1,13 @@
+package at.edu.c02.calculator.logic.operations;
+
+import at.edu.c02.calculator.CalculatorException;
+import at.edu.c02.calculator.MathOperation;
+
+public class Mod implements MathOperation {
+
+    @Override
+    public double calculate(double a, double b) throws CalculatorException {
+        if (b == 0) throw new CalculatorException("Fehler");
+        return a%b;
+    }
+}
